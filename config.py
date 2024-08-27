26d40f6fffed80d8fd049b5c4eec7f8fdfff6223
tatiany <tatianysouza2018@gmail.com>
2024-08-27 18:18:51 -0300

    apenas existindo

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ SECRET_KEY = ENV("SECRET_KEY")
 
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
-        SGBD = 'postgresql',
-        usuario = ENV("USUARIO"),
-        senha = ENV("SENHA"),
-        servidor = 'localhost',
-        database = ENV("DATABASE")
+        SGBD='postgresql',
+        usuario=ENV("USUARIO"),
+        senha=ENV("SENHA"),
+        servidor='localhost',
+        database=ENV("DATABASE")
     )
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
